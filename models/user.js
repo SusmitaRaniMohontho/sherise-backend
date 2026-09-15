4ad4e8f19b5165faf8525049a283ccf96a92cc7f
ankitasristy <ankitasikder0007@gmail.com>
2026-09-15 18:51:47 +0600

    cng by a

--- models/user.js
+++ models/user.js
@@ -16,6 +16,11 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  // প্রোফাইল পেজ সমৃদ্ধ করার জন্য নতুন বায়ো ফিল্ড যুক্ত করা হলো
+  bio: {
+    type: String,
+    default: "",
+  },
   createdAt: {
     type: Date,
     default: Date.now,
