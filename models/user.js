a2465fdeb1be30a5772fe9ae52af3d9cc415cff1
ankitasristy <ankitasikder0007@gmail.com>
2026-09-19 18:09:16 +0600

    cng by a

--- models/user.js
+++ models/user.js
@@ -17,12 +17,12 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  // 🔴 এই 'role' ফিল্ডটি এখানে মিসিং ছিল, তাই এটি নতুন করে যুক্ত করা হলো
+  
   role: {
     type: String,
     default: "SheRise Community Member",
   },
-  // প্রোফাইল পেজ সমৃদ্ধ করার জন্য বায়ো ফিল্ড
+  
   bio: {
     type: String,
     default: "",
@@ -33,7 +33,7 @@ const userSchema = new Schema({
   },
 });
 
-// সেফ মডেল এক্সপোর্ট
+// safe model export
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
